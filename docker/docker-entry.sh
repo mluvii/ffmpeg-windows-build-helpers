d510c6ff6ee887d229dc5cc684066cb0a2a82d8a
Ondřej Vondra <ondrej.vondra@live.com>
2019-02-05 23:59:22 +0100

    building only static version

--- docker/docker-entry.sh
+++ docker/docker-entry.sh
@@ -2,13 +2,12 @@
 set -e
 
 OUTPUTDIR=/output
+COMMIT=31a1d2aa83096c3a07bd2f943bb9be6bda411147 # release/4.1
 
-./cross_compile_ffmpeg.sh --build-ffmpeg-shared=y --build-ffmpeg-static=y --disable-nonfree=n --build-intel-qsv=y --compiler-flavors=win64 --enable-gpl=y --high-bitdepth=n --ffmpeg-git-checkout-version=31a1d2aa83096c3a07bd2f943bb9be6bda411147 # release/4.1
+./cross_compile_ffmpeg.sh --build-ffmpeg-shared=n --build-ffmpeg-static=y --disable-nonfree=n --build-intel-qsv=y --compiler-flavors=win64 --enable-gpl=y --high-bitdepth=n --ffmpeg-git-checkout-version=$COMMIT
 
 mkdir -p $OUTPUTDIR/static/bin
-cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac/ffmpeg.exe $OUTPUTDIR/static/bin
-cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac/ffprobe.exe $OUTPUTDIR/static/bin
-cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac/ffplay.exe $OUTPUTDIR/static/bin
+cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac_$COMMIT/ffmpeg.exe $OUTPUTDIR/static/bin
+cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac_$COMMIT/ffprobe.exe $OUTPUTDIR/static/bin
+cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac_$COMMIT/ffplay.exe $OUTPUTDIR/static/bin
 
-mkdir -p $OUTPUTDIR/shared
-cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac_shared/bin/ $OUTPUTDIR/shared

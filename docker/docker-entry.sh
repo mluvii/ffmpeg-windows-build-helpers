f8426bb4c5451f27bec2bbab26aac7bcdba4240a
Ondřej Vondra <ondrej.vondra@live.com>
2019-02-05 19:42:16 +0100

    compiling from release/4.1 branch

--- docker/docker-entry.sh
+++ docker/docker-entry.sh
@@ -3,7 +3,7 @@ set -e
 
 OUTPUTDIR=/output
 
-./cross_compile_ffmpeg.sh --build-ffmpeg-shared=y --build-ffmpeg-static=y --disable-nonfree=n --build-intel-qsv=y --compiler-flavors=win64 --enable-gpl=y --high-bitdepth=n
+./cross_compile_ffmpeg.sh --build-ffmpeg-shared=y --build-ffmpeg-static=y --disable-nonfree=n --build-intel-qsv=y --compiler-flavors=win64 --enable-gpl=y --high-bitdepth=n --ffmpeg-git-checkout-version=31a1d2aa83096c3a07bd2f943bb9be6bda411147 # release/4.1
 
 mkdir -p $OUTPUTDIR/static/bin
 cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac/ffmpeg.exe $OUTPUTDIR/static/bin
